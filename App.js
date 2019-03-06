618fb23527f0b80a6b53ca51f0b933bc67e39de2
Aruna Gamage <admgbit@gmail.com>
2019-03-06 13:47:12 +0530

    Added Json and Firebase code and Modified gradles

--- App.js
+++ App.js
@@ -10,14 +10,16 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import PropTypes from 'prop-types';
+
+//import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-
-import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 
 type Props = {};
 export default class App extends Component<Props> {
